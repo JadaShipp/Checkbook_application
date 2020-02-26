52b0bb4db43c43305654953fb50145f34944cb5a
jadashipp <jada.l.shipp@gmail.com>
2020-02-26 15:11:52 -0600

    if statements updated

--- python_checkbook.py
+++ python_checkbook.py
@@ -1,48 +1,75 @@
 # python checkbook
-print()
-print()
-print("Salutations! Welcome to your checkbook.")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print()
-print()
 
-print("Please make a choice from the following options:")
+def get_balance():
+    with open("transactions.txt", "r") as f:
+        balance = f.readlines()
+        return(balance)
 
-print("To check your balance, type 1")
-print("To make a withdrawl, type 2")
-print("To make a deposit, type 3")
-print("To exit this application, type 4")
-
-
-def display_balance():
-    user_choice = input("Type choice here : ")
-    while (user_choice.isdigit() == False  
-        or int(user_choice) > 5 
-        or int(user_choice < 0)):   
-        print(f"{user_choice} is an invalid input. Please input a choice from the menu")
-    user_choice = input("Type choice here : ")
-    return user_choice
-
-display_balance()
-
-
-with open("transactions.txt", "r") as f:
-    contents = f.read()
-    print(contents)
+def withdraw():
+    amount = -1*float(input("How much would you like to withdraw? "))
+    with open("transactions.txt", "a") as f:
+        f.write(f"\n{amount}")
+        return amount
 
 def deposit():
     amount = float(input("How much would you like to deposit? "))
-    current_balance = 0
-    print(f"Your new balance is now {amount + current_balance}")
-deposit()
+    with open("transactions.txt", "a") as f:
+        f.write(f"\n{amount}")
+        return amount
 
-def withdraw():
-     amount = float(input("How much would you like to withdraw? "))
-     current_balance = 0
-     print(f"Your new balance is now {current_balance - amount}")
-withdraw()
+while True:
+    print()
+    print()
+    print("Salutations! Welcome to your checkbook.")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print()
+    print()
 
-user_choice = user_choice = input("Type choice here : ")
-    while (user_choice.isdigit() == False  
-        or int(user_choice) > 5 
-        or int(user_choice < 0)): 
+    print("Please make a choice from the following options:")
+
+    print("To check your balance, type 1")
+    print("To make a withdrawl, type 2")
+    print("To make a deposit, type 3")
+    print("To exit this application, type 4")
+
+    user_choice = input("Type choice here : ")
+    #         user_choice = input("Do you want to continue? Type y or Yes. ")
+    #         wants_to_stop = user_choice.lower() not in ["y", "yes"]
+    #         if wants_to_stop:
+    #             break
+
+user_choice = input("Type choice here : ")
+while user_choice.isdigit() == False or int(user_choice) > 5 or int(user_choice) < 0:
+    print(f"{user_choice} is an invalid input. Please input a choice from the menu")
+    user_choice = input("Type choice here : ")
+    break
+if user_choice == 1:
+    get_balance()
+elif user_choice == 2:
+    withdraw()
+elif user_choice == 3:
+    deposit()
+elif user_choice == 4:
+    user_choice = input("Do you want to continue? Type y or Yes. ")
+    wants_to_stop = user_choice.lower() not in ["y", "yes"]
+    if wants_to_stop:
+        break
+
+
+# def get_balance():
+#      with open("transactions.txt", "r") as f:
+#          balance = f.readlines()
+#          return(balance)
+
+
+# def withdraw():
+#     amount = float(input("How much would you like to withdraw? "))
+#     current_balance = 0
+#     print(f"Your new balance is now {current_balance - amount}")
+# withdraw()
+
+# def deposit():
+#     amount = float(input("How much would you like to deposit? "))
+#     current_balance = 0
+#     print(f"Your new balance is now {amount + current_balance}")
+# deposit() 
