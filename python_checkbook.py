9de516cbb3707fa09e898a47c48a46bfdc58c65e
jadashipp <jada.l.shipp@gmail.com>
2020-02-26 15:26:58 -0600

    Showing balance

--- python_checkbook.py
+++ python_checkbook.py
@@ -17,14 +17,16 @@ def deposit():
         f.write(f"\n{amount}")
         return amount
 
+
+print()
+print()
+print("Salutations! Welcome to your checkbook.")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print()
+print()
+
 while True:
     print()
-    print()
-    print("Salutations! Welcome to your checkbook.")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print()
-    print()
-
     print("Please make a choice from the following options:")
 
     print("To check your balance, type 1")
@@ -32,28 +34,27 @@ while True:
     print("To make a deposit, type 3")
     print("To exit this application, type 4")
 
-    user_choice = input("Type choice here : ")
     #         user_choice = input("Do you want to continue? Type y or Yes. ")
     #         wants_to_stop = user_choice.lower() not in ["y", "yes"]
     #         if wants_to_stop:
     #             break
 
-user_choice = input("Type choice here : ")
-while user_choice.isdigit() == False or int(user_choice) > 5 or int(user_choice) < 0:
-    print(f"{user_choice} is an invalid input. Please input a choice from the menu")
     user_choice = input("Type choice here : ")
-    break
-if user_choice == 1:
-    get_balance()
-elif user_choice == 2:
-    withdraw()
-elif user_choice == 3:
-    deposit()
-elif user_choice == 4:
-    user_choice = input("Do you want to continue? Type y or Yes. ")
-    wants_to_stop = user_choice.lower() not in ["y", "yes"]
-    if wants_to_stop:
-        break
+    while user_choice.isdigit() == False or int(user_choice) > 5 or int(user_choice) < 0:
+        print(f"{user_choice} is an invalid input. Please input a choice from the menu")
+        user_choice = input("Type choice here : ")
+    user_choice = int(user_choice)
+    if user_choice == 1:
+        print(get_balance())
+    elif user_choice == 2:
+        withdraw()
+    elif user_choice == 3:
+        deposit()
+    elif user_choice == 4:
+        user_choice = input("Do you want to continue? Type y or Yes. ")
+        wants_to_stop = user_choice.lower() not in ["y", "yes"]
+        if wants_to_stop:
+            break
 
 
 # def get_balance():
